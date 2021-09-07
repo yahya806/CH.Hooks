3eb1a523532a846e04c5c9e9d86476d982c8b862
Yahya <86604180+yahya806@users.noreply.github.com>
2021-09-07 15:48:17 +0200

    Add files via upload

---
+++ New folder (3)/src/Components/MovieCard.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Card} from 'react-bootstrap'
+import ReactStars from 'react-rating-stars-component'
+
+const MovieCard = ({movie}) => {
+    return (
+        <div>
+           <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={movie.posterurl} />
+  <Card.Body>
+    <Card.Title>{movie.name}</Card.Title>
+    <Card.Text>
+      {movie.description}
+    </Card.Text>
+    
+  </Card.Body>
+  <ReactStars
+    count={5}
+    size={24}
+    activeColor="#ffd700"
+    edit={false}
+    value={movie.rating}
+  />
+</Card>
+        </div>
+    )
+}
+
+export default MovieCard
